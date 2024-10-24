4819d9faa13a6f64eec7a441b3856a6222e39761
mjohra <mehjabin.johra00@gmail.com>
2024-10-24 13:08:05 +0600

    Array Declaration,update,index value print

---
+++ JS-Array/declare-array.js
@@ -0,0 +1,13 @@
+/*
+1. Declare an array
+--Declare an array with 5 elements containing fruits
+--console log the 3rd index element
+--change the value of the 2nd index element to Kiwi
+--console log the final array
+
+*/
+
+let fruits = ['Mango',' Lichi', 'Pineapple', 'Jackfruit', 'Papaya'];
+console.log(fruits[3]);
+fruits[2]='Kiwi';
+console.log(fruits);
