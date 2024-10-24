bf0e15600882e524cf031e023bc4b6903a2b70b7
mjohra <mehjabin.johra00@gmail.com>
2024-10-24 14:02:56 +0600

    Join two array using concat

---
+++ JS-Array/combining-array.js
@@ -0,0 +1,15 @@
+/*
+Combining Arrays
+Instructions:
+--Create two arrays of your choice.
+--Use the concat method to combine the two arrays into a new array.
+--Print both the original arrays and the combined array using console.log().
+*/
+
+let books = ["Ikigai", "Paradoxical Sajid", "The Fountainhead"];
+let numbers=[11,22,33,44,55,66,77];
+let tourist_destination=['MECCA','Medina','Kashmir'];
+console.log(books);
+console.log(numbers);
+console.log(numbers.concat(books));
+console.log(tourist_destination.concat(books));
