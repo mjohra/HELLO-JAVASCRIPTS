aa63fdaa9334223938b7ef2b9ad5f05c8d2037c2
mjohra <mehjabin.johra00@gmail.com>
2024-10-24 13:21:16 +0600

    Array element add and remove

---
+++ JS-Array/add-remove-element.js
@@ -0,0 +1,18 @@
+/*
+***Add or remove elements
+-Declare an array of 3 tourist destinations
+-Add a new tourist destination to your tourist array
+-Add two more to your array
+-Remove the last tourist destination you have added
+-display the final array as output
+
+*/
+
+let tourist_destination=['MECCA','Medina','Kashmir'];
+console.log(tourist_destination);
+tourist_destination.push('Dubai');
+console.log(tourist_destination);
+tourist_destination.push('Turkey','Pakistan');
+console.log(tourist_destination);
+tourist_destination.pop();
+console.log(tourist_destination);
