de253683b94dee0072be207f84d6f36616887a3b
mjohra <mehjabin.johra00@gmail.com>
2024-10-26 08:59:22 +0600

    sum of odd even numbers

---
+++ JS-Loops/For/sum-odd-even.js
@@ -0,0 +1,29 @@
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 91 to 129.
+
+ */
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 51 to 85.
+
+ */
+let oddSum = 0;
+for (let i = 91; i <= 129; i++) {
+  if (i % 2 == 1) {
+    oddSum += i;
+  }
+}
+console.log("Sum of odd number is " , oddSum);
+
+let evenSum = 0;
+for (let i = 51; i <= 85; i++) {
+  if (i % 2 == 1) {
+    evenSum += i;
+  }
+}
+console.log("Sum of even number is " , evenSum);
