332b0ca9f5d5ef90ffb9387876b24e5bc1b2da2f
mjohra <mehjabin.johra00@gmail.com>
2024-10-26 09:09:24 +0600

    multiplication table for number 9

--- JS-Loops/For/sum-odd-even.js
+++ JS-Loops/For/sum-odd-even.js
@@ -5,6 +5,15 @@ Subtask-1:
 Display sum of all the odd numbers from 91 to 129.
 
  */
+
+let oddSum = 0;
+for (let i = 91; i = 129; i++) {
+  if (i % 2 == 1) {
+    oddSum += i;
+  }
+}
+console.log("Sum of odd number is ", oddSum);
+
 /***
 
 Subtask-2:
@@ -12,13 +21,6 @@ Subtask-2:
 Display sum of all the even numbers from 51 to 85.
 
  */
-let oddSum = 0;
-for (let i = 91; i <= 129; i++) {
-  if (i % 2 == 1) {
-    oddSum += i;
-  }
-}
-console.log("Sum of odd number is " , oddSum);
 
 let evenSum = 0;
 for (let i = 51; i <= 85; i++) {
@@ -26,4 +28,4 @@ for (let i = 51; i <= 85; i++) {
     evenSum += i;
   }
 }
-console.log("Sum of even number is " , evenSum);
+console.log("Sum of even number is ", evenSum);
