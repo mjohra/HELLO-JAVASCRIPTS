89145afdd9117e8be706fd5dec3c4afe48884d93
mjohra <mehjabin.johra00@gmail.com>
2024-10-26 09:16:38 +0600

    countdown timer that counts down from 81 to 65

---
+++ JS-Loops/For/countDown.js
@@ -0,0 +1,13 @@
+/***
+
+Implement a countdown timer that counts down from 81 to 65.
+
+ */
+
+const endCount = 65;
+
+// Loop to count down from 81 to 65
+for (let count = 81; count >= 65; count--) {
+  console.log(count);
+}
+console.log("Countdown finished!");
