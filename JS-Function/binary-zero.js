da8858a957b9ecba4fff67911cc4b50319e5dbb2
mjohra <mehjabin.johra00@gmail.com>
2024-11-03 08:11:43 +0600

    binary zero count

--- JS-Function/binary-zero.js
+++ JS-Function/binary-zero.js
@@ -4,17 +4,20 @@ Write a function called count_zero() which will take a binary string (Binary str
 
 */
 
-function count_zero(binary_num) {
-  let count = 0;
-  for (let i = 0; i < binary_num.length; i++) {
-    if (binary_num[i].includes("0")) {
-      count++;
+function count_zero(binary_num){
+    let count=0;
+    for(let i=0;i<binary_num.length;i++){
+  if (binary_num.includes('0')){
+        count++;
     }
-  }
-
-  return count;
+    }
+  
+return count;
 }
 
 console.log(count_zero("101010")); // Output: 3
-console.log(count_zero("1111")); // Output: 0
-console.log(count_zero("00000")); // Output: 5
+console.log(count_zero("1111"));   // Output: 0
+console.log(count_zero("00000"));  // Output: 5
+
+
+
